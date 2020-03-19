cdb1d825eb8ee8c39d1a1837a4a40e0f407b41a6
Akshay Bharadwaj <akshaybharadwaj@Akshays-MacBook-Pro.local>
2020-03-18 21:29:08 -0400

    first edit

--- jobapp/pages/index.tsx
+++ jobapp/pages/index.tsx
@@ -3,13 +3,13 @@ import Head from 'next/head'
 const Home = () => (
   <div className="container">
     <Head>
-      <title>Create Next App</title>
+      <title>JobApp</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <main>
       <h1 className="title">
-        Welcome to Next.js!
+        Welcome to JobApp!
       </h1>
 
       <p className="description">
@@ -18,7 +18,7 @@ const Home = () => (
     </main>
 
     <footer>
-      This is a footer 
+      This is a footer!
     </footer>
   </div>
 )
