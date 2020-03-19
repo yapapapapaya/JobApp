59d868efd4d56b97fae1e36b7bc22a742ca37add
Akshay Bharadwaj <akshaybharadwaj@Akshays-MacBook-Pro.local>
2020-03-18 21:24:53 -0400

    npm init and initial packages

---
+++ jobapp/pages/index.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+
+const Home = () => (
+  <div className="container">
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main>
+      <h1 className="title">
+        Welcome to Next.js!
+      </h1>
+
+      <p className="description">
+        Get started by editing <code>pages/index.js</code>
+      </p>
+    </main>
+
+    <footer>
+      This is a footer 
+    </footer>
+  </div>
+)
+export default Home
