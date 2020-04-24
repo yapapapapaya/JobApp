9c9991e48b30f33e00940f58636660b5d850093e
akshaycbharadwaj <38265950+akshaycb@users.noreply.github.com>
2020-04-24 14:11:49 -0400

    common header and footer implemented to all the pages

--- jobapp/pages/index.tsx
+++ jobapp/pages/index.tsx
@@ -1,25 +1,14 @@
 import React from 'react'
-import Login from './Login/Login'
-import Header from '../src/Header/Header'
-import Footer from '../src/Footer/Footer'
 
-const Home = () => (
-  <div className="container">
-    <head>
-      <title>JobApp</title>
-      <link rel="icon" href="/favicon.ico" />
-    </head>
-
-    <main>
-      <header>
-        <Header />
-      </header>
-      <Login />
-      <footer>
-        <Footer />
-    </footer>
-    </main>
-    
-  </div>
-)
+const Home = () => {
+  console.log('this is index page')
+  return (
+    <div className="container">
+      <main>
+      </main>
+        
+    </div>
+  )
+  
+}
 export default Home
